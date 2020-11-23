18ddac8d77ecec0fc565a3af65998d411b16d97d
Adam Zajaczkowski <adamzaj16@gmail.com>
2020-11-23 21:13:52 +0100

    Initial

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,9 +1,14 @@
 const footer = document.querySelector('footer');
-//Skrypt na wyjeżdzanie UI (mobilka)
-let wysokoscStrony = window.innerHeight;
+const przyciskKontakt = document.querySelector('div.kontakt');
+const menuKontakt = document.querySelector('div.menu-kontakt');
+const przyciskZamknij = document.querySelector('i.fa-times');
+const aktualnosciFacebook = document.querySelector('.promo-strona')
+let wysokoscStrony = window.innerHeight; //nie używać bo jest nadpisywana
+let szerokoscStrony = window.innerWidth;
 
+//Skrypt na wyjeżdzanie UI (mobilka)
 window.addEventListener('scroll', () => {
-    if (window.innerWidth < 720 && !menuKontakt.classList.contains('widoczne')){
+    if (szerokoscStrony < 720 && !menuKontakt.classList.contains('widoczne')){
     const aktualnaWysokoscNaStronie = window.pageYOffset;
     if (wysokoscStrony > aktualnaWysokoscNaStronie) {
         footer.style = "display: block";
@@ -12,18 +17,36 @@ window.addEventListener('scroll', () => {
     } else {
         footer.style = "display: none";
     }
-
     wysokoscStrony = aktualnaWysokoscNaStronie
 }
 })
 
 //Skrypt na wysunięcie zakładki 'Kontakt'
-const przyciskKontakt = document.querySelector('div.kontakt');
-const menuKontakt = document.querySelector('div.menu-kontakt');
-const przyciskZamknij = document.querySelector('i.fa-times');
 przyciskKontakt.addEventListener('click', () => {
     menuKontakt.classList.toggle('widoczne')
 })
 przyciskZamknij.addEventListener('click', ()=>{
     menuKontakt.classList.remove('widoczne')
 })
+
+window.addEventListener('scroll', () => {
+    if(window.innerWidth > 720 && menuKontakt.classList.contains('widoczne')) {
+        menuKontakt.classList.remove('widoczne')
+    }
+})
+
+function lastSingleLetterToNewLine(el){
+    let result;
+    el.forEach((element, i)=>{
+       
+       if(!element.innerHTML.match(/[{}]|<script|^\n$/gi)){
+        result = element.innerHTML.replace(/ (.) /gi, " "+'\$1'+"&nbsp");
+       }
+       element.innerHTML = result;
+       console.log(result);
+    });
+  }
+  
+  let el = document.querySelectorAll("h1, h2, h3, h4, h5, p, span, .text");
+  lastSingleLetterToNewLine(el);
+
